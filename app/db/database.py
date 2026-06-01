9b2c5658b8a36a2f4644f2d2c97d5663f2c4458c
Ayush Siawal <ayush-codes11@users.noreply.github.com>
2026-06-02 00:08:11 +0530

    Add PostgreSQL persistence volume

---
+++ app/db/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = (
+    "postgresql://devuser:devpassword@db:5432/taskdb"
+)
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
--- app/main.py
+++ app/main.py
@@ -1,11 +1,64 @@
 from fastapi import FastAPI
 
+from app.db.database import engine
+from app.models import Base
+
+
+from sqlalchemy.orm import Session
+
+from app.db.database import SessionLocal
+from app.models import Task
+from app.schemas import TaskCreate, TaskResponse
+
+
+from fastapi import Depends
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 @app.get("/")
-def home():
+def root():
     return {"message": "Task API running"}
 
 @app.get("/health")
 def health():
     return {"status": "healthy"}
+
+
+
+
+
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+
+@app.post("/tasks", response_model=TaskResponse)
+def create_task(
+    task: TaskCreate,
+    db: Session = Depends(get_db)
+):
+    db_task = Task(title=task.title)
+
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+
+    return db_task
+
+
+
+
+@app.get("/tasks", response_model=list[TaskResponse])
+def list_tasks(
+    db: Session = Depends(get_db)
+):
+    return db.query(Task).all()
